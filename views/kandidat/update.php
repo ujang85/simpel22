faff0fde8e7b85f2c663ba8c1fe8785ab98776c6
System Administrator <root@muhammads-MacBook-Pro.local>
2022-08-31 14:07:55 +0700

    tambah info kriteria penilaian2

--- views/kandidat/update.php
+++ views/kandidat/update.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $model app\models\Kandidat */
 
 $this->title = 'Update Kandidat: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Kandidats', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Kandidat', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
